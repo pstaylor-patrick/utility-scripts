f0da5cb380e18bfa908308d2351145ccb84c6536
Patrick Taylor <patrick.taylor@daveramsey.com>
2021-01-15 15:14:42 -0600

    add python scripts

---
+++ python-scripts/delete-all-items-from-dynamodb.py
@@ -0,0 +1,17 @@
+import boto3
+import sys
+
+
+def main():
+    dynamodb_table = boto3.resource("dynamodb").Table("rt-survey-recipients")
+    with dynamodb_table.batch_writer() as batch:
+        for item in dynamodb_table.scan().get("Items", []):
+            batch.delete_item(
+                Key={
+                    "pk": item.get("pk"),
+                    "sk": item.get("sk")
+                }
+            )
+
+
+main()
