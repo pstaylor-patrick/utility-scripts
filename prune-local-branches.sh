2b74b4f7252c1665c253e12d6a6975fcc1244fb4
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2023-10-25 12:13:44 -0500

    Revert "update prunelocal to refresh env files"
    This reverts commit f9ca01379c8dd3ff4f43e1e087992e4c3b8fe47e.

--- prune-local-branches.sh
+++ prune-local-branches.sh
@@ -35,21 +35,4 @@ git fetch
 git branch -D $temp_branch
 git branch --list --all
 
-# region - refresh env files
-declare -a envs=()
-while IFS= read -r line; do
-    envs+=("$line")
-done < <(find $(dirname $(git rev-parse --show-toplevel)) -type f -name ".env*" -mindepth 1 -maxdepth 1)
-declare -a apps=()
-while IFS= read -r line; do
-    apps+=("$line")
-done < <(find $(git rev-parse --show-toplevel) -type d -name "*-app" -mindepth 1 -maxdepth 1)
-for app in "${apps[@]}"; do
-  for env in "${envs[@]}"; do
-    echo "$env - $app"
-    cp $env $app
-  done
-done
-# endregion
-
 echo "************ end prunelocal ************"
